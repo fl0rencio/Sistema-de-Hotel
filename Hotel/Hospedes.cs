5874a379fc3e2161796e078ba207819117bbfb4f
Wendel Florencio <wendel.github@gmail.com>
2022-11-17 14:30:55 -0300

    .

---
+++ Hotel/Hospedes.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace Hotel
+{
+    public class Hospedes
+    {
+
+        
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Hospedes( string nome, string email)
+        {
+            
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return "NOME: " + Nome + 
+                "EMAIL: " + Email;
+        }
+    }
+}
---
+++ Hotel/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+
+namespace Hotel
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+
+            Hospedes[] hospedes = new Hospedes[9];
+            Console.Write("DIGITE QUANTOS QUARTOS DESEJA ALUGAR: " );
+            int quartos = int.Parse( Console.ReadLine());
+            Console.WriteLine();
+
+
+
+            for (int i = 1; i <= quartos; i++)
+            {
+                Console.WriteLine("HOSPEDE: " + i);
+                Console.Write("DIGITE SEU NOME: ");
+                string nome = Console.ReadLine();
+                Console.WriteLine();
+
+                Console.Write("DIGITE SEU EMAIL: ");
+                string email = Console.ReadLine();
+                Console.WriteLine();
+
+                Console.Write("DIGITE  O NUMERO DO QUARTO DESEJADO: ");
+                int quarto = int.Parse( Console.ReadLine());
+                Console.WriteLine();
+
+                hospedes[quarto] = new Hospedes (nome,email);
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine("QUARTOS OCUPADOS:");
+            for(int i = 0; i < 9; i++)
+            {
+                if(hospedes[i] != null)
+                {
+                    Console.WriteLine("QUARTO NUMERO " +i + " : " + hospedes[i]);
+                }
+            }
+        }
+    }
+}
